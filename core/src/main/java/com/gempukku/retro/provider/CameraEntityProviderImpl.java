b361b578c8bf1f1fe3f63b119859a5beda5b79ad
marcin.sciesinski <Marcin.Sciesinski@stash.synchronoss.net>
2018-12-31 13:13:40 -0800

    Camera is loaded from scene

--- core/src/main/java/com/gempukku/retro/provider/CameraEntityProviderImpl.java
+++ core/src/main/java/com/gempukku/retro/provider/CameraEntityProviderImpl.java
@@ -8,16 +8,11 @@ import com.gempukku.secsy.entity.index.EntityIndex;
 import com.gempukku.secsy.entity.index.EntityIndexManager;
 import com.gempukku.secsy.gaming.camera2d.component.Camera2DComponent;
 import com.gempukku.secsy.gaming.rendering.pipeline.CameraEntityProvider;
-import com.gempukku.secsy.gaming.spawn.SpawnManager;
 
 @RegisterSystem(shared = CameraEntityProvider.class)
 public class CameraEntityProviderImpl extends AbstractLifeCycleSystem implements CameraEntityProvider {
     @Inject
-    private SpawnManager spawnManager;
-    @Inject
     private EntityIndexManager entityIndexManager;
-
-    private EntityRef gameCameraEntity;
 
     private EntityIndex cameraIndex;
 
