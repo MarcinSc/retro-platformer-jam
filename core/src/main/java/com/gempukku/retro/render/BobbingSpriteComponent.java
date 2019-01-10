b949d445042b15078d6091b9264c9760e9c16673
marcin.sciesinski <Marcin.Sciesinski@stash.synchronoss.net>
2019-01-09 21:57:27 -0800

    Cosmetic changes for editor

--- core/src/main/java/com/gempukku/retro/render/BobbingSpriteComponent.java
+++ core/src/main/java/com/gempukku/retro/render/BobbingSpriteComponent.java
@@ -1,8 +1,14 @@
 package com.gempukku.retro.render;
 
+import com.gempukku.secsy.gaming.editor.generic.EditorFields;
+import com.gempukku.secsy.gaming.editor.generic.EditorName;
+import com.gempukku.secsy.gaming.editor.generic.type.EditorField;
 import com.gempukku.secsy.gaming.rendering.sprite.SpriteComponent;
 
+@EditorFields({"fileName", "bobbingAmplitude", "priority", "leftPerc", "rightPerc", "downPerc", "upPerc"})
+@EditorName("Bobbing Sprite")
 public interface BobbingSpriteComponent extends SpriteComponent {
+    @EditorField("Amplitude")
     float getBobbingAmplitude();
 
     void setBobbingAmplitude(float bobbingAmplitude);
--- secsy-gaming/src/main/java/com/gempukku/secsy/gaming/rendering/sprite/SpriteComponent.java
+++ secsy-gaming/src/main/java/com/gempukku/secsy/gaming/rendering/sprite/SpriteComponent.java
@@ -1,12 +1,14 @@
 package com.gempukku.secsy.gaming.rendering.sprite;
 
 import com.gempukku.secsy.gaming.editor.generic.EditorFields;
+import com.gempukku.secsy.gaming.editor.generic.type.EditorField;
 import com.gempukku.secsy.gaming.editor.generic.type.FileExistsValidator;
 import com.gempukku.secsy.gaming.editor.generic.type.StringValidator;
 
 @EditorFields({"fileName", "priority", "leftPerc", "rightPerc", "downPerc", "upPerc"})
 public interface SpriteComponent extends DisplayableSpriteComponent {
     @StringValidator(FileExistsValidator.class)
+    @EditorField("Path")
     String getFileName();
 
     void setFileName(String fileName);
