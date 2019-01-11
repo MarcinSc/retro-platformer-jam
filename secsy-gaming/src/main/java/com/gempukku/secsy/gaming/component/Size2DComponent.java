93d6195cc6cdc2ad08753f027463085dbffd07df
marcin.sciesinski <Marcin.Sciesinski@stash.synchronoss.net>
2019-01-10 21:29:07 -0800

    Modifying components

--- secsy-gaming/src/main/java/com/gempukku/secsy/gaming/component/Size2DComponent.java
+++ secsy-gaming/src/main/java/com/gempukku/secsy/gaming/component/Size2DComponent.java
@@ -4,6 +4,7 @@ import com.gempukku.secsy.entity.Component;
 import com.gempukku.secsy.gaming.editor.generic.EditorFields;
 import com.gempukku.secsy.gaming.editor.generic.EditorName;
 import com.gempukku.secsy.gaming.editor.generic.type.EditorField;
+import com.gempukku.secsy.gaming.editor.generic.type.FloatRange;
 
 @EditorFields({"width", "height", "anchorX", "anchorY"})
 @EditorName("Size")
@@ -18,12 +19,14 @@ public interface Size2DComponent extends Component {
 
     // Percentage of width for anchor, i.e. if anchor is on left, it's 0, if on right 1, if in the middle 0.5
     @EditorField("Anchor X")
+    @FloatRange(min = 0, max = 1)
     float getAnchorX();
 
     void setAnchorX(float anchorX);
 
     // Percentage of height for anchor, i.e. if anchor is on the bottom, it's 0, if on top 1, if in the middle 0.5
     @EditorField("Anchor Y")
+    @FloatRange(min = 0, max = 1)
     float getAnchorY();
 
     void setAnchorY(float anchorY);
